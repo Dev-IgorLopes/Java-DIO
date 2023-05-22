472d4e3feb37209847ac1eefa197c94bba1bd1ec
Dev-IgorLopes <igorlopes.adm1@gmail.com>
2023-05-21 23:41:30 -0300

    AtividadeDIO

---
+++ src/Exec1.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Scanner;
+
+public class Exec1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+       
+    Mensagem.mensagem(2);
+    Calculadora.soma(34, 45);
+    Calculadora.subtracao(56, 32);
+    Calculadora.multiplicacao(4, 6);
+    Calculadora.divisao(32, 12);
+        
+    Emprestimo.calcular(45000, Emprestimo.getDuasParcelas());
+    Emprestimo.calcular(45000, Emprestimo.getTresParcelas());
+    Emprestimo.calcular(45000, 6);
+}
+}
---
+++ src/Mensagem.java
@@ -0,0 +1,17 @@
+import java.util.Calendar;
+
+public class Mensagem {
+    public static void mensagem(int horas) {
+        
+
+        if(horas> 6 && horas<12){
+            System.out.println("Bom dia");
+        }else if(horas>12 && horas<18){
+            System.out.println("Boa tarde");
+        }else if(horas>18 && horas<00){
+            System.out.println("Boa Noite");
+        }else{
+            System.out.println("Madrugada");
+        }
+    }
+}
